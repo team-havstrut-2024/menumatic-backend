7fcdb3a370f0b1412754a70e4a79d41a88fa0d97
gustav-landberg <58774673+gustav-landberg@users.noreply.github.com>
2024-04-23 08:17:15 +0200

    initial commit

---
+++ src/main/java/com/havstrut/menumatic/MenumaticApplication.java
@@ -0,0 +1,13 @@
+package com.havstrut.menumatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenumaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenumaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/havstrut/menumatic/MenumaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havstrut.menumatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenumaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
