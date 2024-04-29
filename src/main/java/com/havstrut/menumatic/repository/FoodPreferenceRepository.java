7197cf9002b7ca83d32691e505e61f277b9a2870
Gustav Landberg <landbergg@outlook.com>
2024-04-29 11:38:26 +0200

    Added two functions in service

--- src/main/java/com/havstrut/menumatic/repository/FoodPreferenceRepository.java
+++ src/main/java/com/havstrut/menumatic/repository/FoodPreferenceRepository.java
@@ -1,9 +1,19 @@
 package com.havstrut.menumatic.repository;
 
 import com.havstrut.menumatic.model.FoodPreference;
+import com.havstrut.menumatic.model.RecipeMealplan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FoodPreferenceRepository extends JpaRepository<FoodPreference, Integer> {
+
+
+    // Get list of foodpreferences
+    @Query("SELECT fp FROM FoodPreference fp WHERE fp.userId = ?1")
+    List<FoodPreference> findByUserId(String id);
+
 }
--- src/main/java/com/havstrut/menumatic/service/FoodPreferenceService.java
+++ src/main/java/com/havstrut/menumatic/service/FoodPreferenceService.java
@@ -1,8 +1,11 @@
 package com.havstrut.menumatic.service;
 
+import com.havstrut.menumatic.model.FoodPreference;
 import com.havstrut.menumatic.repository.FoodPreferenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodPreferenceService {
@@ -13,4 +16,41 @@ public class FoodPreferenceService {
     public FoodPreferenceService(FoodPreferenceRepository foodPreferenceRepository) {
         this.foodPreferenceRepository = foodPreferenceRepository;
     }
+
+    // Create a foodpreference if it is not already present
+    public void CreateFoodPreference(String uid, String preference) throws Exception{
+        List<FoodPreference> table = foodPreferenceRepository.findByUserId(uid);
+        for (FoodPreference row : table) {
+            if (row.equals(preference))
+                    throw new Exception("Food preference \"" + preference + "\" already exists for user " + uid);
+        }
+        FoodPreference fp = new FoodPreference(uid, preference);
+        this.foodPreferenceRepository.save(fp); // store new foodpreference in table
+    }
+    // Delete
+    public void deleteFoodPreference(String uid, String preference) throws Exception {
+        List<FoodPreference> table = foodPreferenceRepository.findByUserId(uid);
+        boolean exists = false;
+        for (FoodPreference row : table) {
+            if (row.equals(preference)) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            throw new Exception("Food preference \"" + preference + "\" not present for user " + uid);
+        }
+        FoodPreference fp = new FoodPreference(uid, preference);
+            this.foodPreferenceRepository.delete(fp);
+}
+
+    // getAllFoodPreferencesForUser
+    public void getAllFoodPreferencesForUser
+
+
+
+
+
+
+
 }
