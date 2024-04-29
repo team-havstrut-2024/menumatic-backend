9be0ce799723478e4814cf114d6bfc2b8de4ac64
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-04-29 16:34:00 +0200

    Update FoodPreferenceController.java

--- src/main/java/com/havstrut/menumatic/controller/FoodPreferenceController.java
+++ src/main/java/com/havstrut/menumatic/controller/FoodPreferenceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/") //byta till "/registeredUsers" ?
+@RequestMapping("/api/food-preferences/") //byta till "/registeredUsers" ?
 
 public class FoodPreferenceController {
 
