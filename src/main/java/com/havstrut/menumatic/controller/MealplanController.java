6bf19de37758e0a2eccf593a0ca5d073b4fa9e8c
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-04-29 15:21:36 +0200

    Update

--- src/main/java/com/havstrut/menumatic/controller/MealplanController.java
+++ src/main/java/com/havstrut/menumatic/controller/MealplanController.java
@@ -33,7 +33,7 @@ public class MealplanController {
     public void deleteMealplan(@RequestHeader("User-id") String uid, @RequestBody String json) throws Exception {
         String newUid = uid.replace('"', ' ').trim();
         System.out.println(newUid);
-        //System.out.println(json);
+        System.out.println(json);
 
         Map<String, Object> jsonMap = new HashMap<>();
 
@@ -41,14 +41,16 @@ public class MealplanController {
 
         System.out.println(("This is the json object: " + jsonMap));
 
-
         String mealplanId = (String) jsonMap.get("mealplanId");
-        System.out.println(("This is the mealplan id: " + mealplanId));
 
-        /*List<Map<String, Object>> recipes = (List<Map<String, Object>>) jsonMap.get("recipes");
-        System.out.println("This is the recipes object: " + recipes);
-        registeredUserService.addNewStudent(newUid);
-        int mealplan_id = mealplanService.addNewMealplan(planName, Timestamp.valueOf(LocalDateTime.now()) ,newUid);
+        System.out.println("This is the mealplan id: " + mealplanId);
+        System.out.println(mealplanId instanceof String);
+        //System.out.println(mealplanId instanceof int);
+
+        /*List<Map<String, Object>> recipes = (List<Map<String, Object>>) jsonMap.get("mealplanId");
+        System.out.println("This is the recipes object: " + recipes);*/
+        /*registeredUserService.addNewStudent(newUid);
+        int mealplan_id = mealplanService.addNewMealplan(planName, Timestamp.valueOf(LocalDateTime.now()), newUid);
         for (Map<String, Object> map : recipes) {
             String rName = (String) map.get("name");
             //System.out.println(recipes.get(0));
@@ -56,20 +58,14 @@ public class MealplanController {
             Object rPortion = map.get("portion");
             int rId = (int) map.get("id");
 //            System.out.println(("This is the id of the recipe: " + rId));
-            if (rPortion.getClass() == String.class)
-            {
-                recipeService.addNewRecipe(rId,  rName, Integer.parseInt((String)rPortion));
-            }
-            else{
-                recipeService.addNewRecipe(rId,  rName, (int) rPortion);
+            if (rPortion.getClass() == String.class) {
+                recipeService.addNewRecipe(rId, rName, Integer.parseInt((String) rPortion));
+            } else {
+                recipeService.addNewRecipe(rId, rName, (int) rPortion);
             }
 
             recipeMealplanService.addNewRecipeMealplan(rId, mealplan_id);
-        }
-
+        }*/
     }
 
-
-    }*/
-    }
 }
