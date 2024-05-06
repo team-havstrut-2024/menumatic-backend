ed15e8ef9636d39a7c59be49e2ae2d0af3396637
Teoman Köylüoglu <Teoman.sevh@outlook.com>
2024-05-06 15:16:58 +0200

    Fix add new user for Food preference

--- src/main/java/com/havstrut/menumatic/controller/FoodPreferenceController.java
+++ src/main/java/com/havstrut/menumatic/controller/FoodPreferenceController.java
@@ -2,6 +2,7 @@ package com.havstrut.menumatic.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.havstrut.menumatic.service.FoodPreferenceService;
+import com.havstrut.menumatic.service.RegisteredUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,14 @@ import java.util.List;
 public class FoodPreferenceController {
 
     private FoodPreferenceService foodPreferenceService;
+    private RegisteredUserService registeredUserService;
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public FoodPreferenceController(FoodPreferenceService foodPreferenceService, ObjectMapper objectMapper) {
+    public FoodPreferenceController(FoodPreferenceService foodPreferenceService, ObjectMapper objectMapper, RegisteredUserService registeredUserService) {
         this.foodPreferenceService = foodPreferenceService;
         this.objectMapper = objectMapper;
+        this.registeredUserService = registeredUserService;
     }
 
     /*
@@ -39,15 +42,17 @@ public class FoodPreferenceController {
     @CrossOrigin
     @PostMapping("set/")
     public void setUserPreference(@RequestHeader("User-id") String uid, @RequestBody List<String> parameters) {
-        foodPreferenceService.nuke(uid);
+        String newUid = uid.replace('"', ' ').trim();
+        registeredUserService.addNewStudent(newUid);
+        foodPreferenceService.nuke(newUid);
         for (String s : parameters) {
             System.out.println("s");
             try {
-            foodPreferenceService.createFoodPreference(uid, s);
+            foodPreferenceService.createFoodPreference(newUid, s);
             }
             catch(Exception e) {
                 // Log or handle the exception appropriately
-                System.out.println("Error creating preference for user: " + uid + ", item: " + s + ", error: " + e.getMessage());
+                System.out.println("Error creating preference for user: " + newUid + ", item: " + s + ", error: " + e.getMessage());
             }
         }
     }
