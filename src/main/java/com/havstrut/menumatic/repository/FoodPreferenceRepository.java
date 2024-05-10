35d05443623441b096961e19a32bb73616cf36ba
Gustav Landberg <landbergg@outlook.com>
2024-05-10 10:23:01 +0200

    Refactoring of repository layer complete

--- src/main/java/com/havstrut/menumatic/repository/FoodPreferenceRepository.java
+++ src/main/java/com/havstrut/menumatic/repository/FoodPreferenceRepository.java
@@ -1,7 +1,6 @@
 package com.havstrut.menumatic.repository;
 
 import com.havstrut.menumatic.model.FoodPreference;
-import com.havstrut.menumatic.model.RecipeMealplan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
